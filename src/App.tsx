03abe51bd659b7b74d8b3956c69b9f030d0fb288
nyxfor13days <nyxfor13days@gmail.com>
2022-11-07 01:13:43 +0530

    Created UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 const App = () => {
-  return <div className="App">App</div>;
+  return (
+    <div className="container flex items-center justify-center h-screen">
+      <div className="flex flex-col items-center gap-6">
+        <div className="h-96 w-96 rounded-2xl bg-rose-900 shadow-md" />
+
+        {/* Button Group */}
+        <div className="w-full flex justify-between gap-4">
+          <button>Button</button>
+          <button>Button</button>
+          <button>Button</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,18 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
     extend: {
       colors: ({ colors }) => ({
-        background: colors.zinc[300],
-        onBackground: colors.zinc[900],
-        success: colors.emerald[600],
-        error: colors.rose[600],
+        background: colors.zinc[900],
+        onBackground: colors.zinc[300],
+        surface: colors.zinc[800],
+        onSurface: colors.zinc[300],
+        success: colors.emerald[400],
+        error: colors.rose[400],
       }),
       fontFamily: {
         sans: ["Inter", "sans-serif"],
