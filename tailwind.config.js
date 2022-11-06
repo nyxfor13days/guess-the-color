29d378c0ae04d46e625b591385bef62cf9c620f0
nyxfor13days <nyxfor13days@gmail.com>
2022-11-07 00:36:14 +0530

    Added Base Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: ({ colors }) => ({
+        background: colors.zinc[300],
+        onBackground: colors.zinc[900],
+        success: colors.emerald[400],
+        onSuccess: colors.emerald[900],
+        error: colors.rose[400],
+        onError: colors.rose[900],
+      }),
+    },
   },
   plugins: [],
 };
